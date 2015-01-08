748cf5ba17f817636970af4dc41ed9dae6582b3f
Matthias Klumpp <matthias@tenstral.net>
2015-01-08 21:14:56 +0100

    Add projects table

---
+++ plugins/projects_table/lib/projects_table.rb
@@ -0,0 +1,15 @@
+module ProjectsTable
+  def self.included(base)
+    base.send(:include, InstanceMethods)
+    base.class_eval do
+      alias_method_chain :render_project_hierarchy, :projects_table
+    end
+  end
+
+  module InstanceMethods
+    def render_project_hierarchy_with_projects_table(projects)
+      tlprojects = projects.select { |p| p.level == 0 }
+      render('table', :projects => tlprojects.sort_by(&:lft))
+    end
+  end
+end
